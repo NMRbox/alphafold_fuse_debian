db57ab8b1ee5fd4c684415e6fafe30fc77f37026
Jon Wedell <jon@jonwedell.com>
2022-10-27 12:39:57 -0500

    Improvements for running index builder in different environments

--- db_builder.py
+++ db_builder.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import sqlite3
 import tarfile
 import subprocess
 
 
-def get_files():
+def get_files(root_dir: str):
     # Populate DB
-    with os.scandir('proteomes') as it:
+    with os.scandir(root_dir) as it:
         for entry in it:
             if entry.name.endswith('.tar'):
                 taxonomy_id = entry.name.replace('proteome-tax_id-', '').replace('-0_v3.tar', '')
@@ -18,8 +19,9 @@ def get_files():
                             yield taxonomy_id, file.name.replace('-F1-model_v3.cif.gz', '')
 
 
-def get_pdb_mappings():
-    if not os.path.exists('idmapping.pdb'):
+def get_pdb_mappings(download=False):
+    if not os.path.exists('idmapping.pdb') or download:
+        print("Downloading Uniprot<->PDB id mapping file...")
         url = 'https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping.dat.gz'
         cmd = f'curl {url} | gzip -dc | grep PDB > idmapping.pdb'
         subprocess.run(cmd, shell=True, check=True)
@@ -32,25 +34,42 @@ def get_pdb_mappings():
                 break
 
 
-with sqlite3.connect('test.sqlite') as sqlite_conn:
-    cursor = sqlite_conn.cursor()
+def create_or_update_sqlite(args: argparse.Namespace):
+    with sqlite3.connect('alphafold.sqlite') as sqlite_conn:
+        cursor = sqlite_conn.cursor()
 
-    # Set up taxonomy<->uniprot DB
-    cursor.execute('DROP TABLE IF EXISTS taxonomy_tmp;')
-    cursor.execute('CREATE TABLE taxonomy_tmp (taxonomy_id text, uniprot_id text);')
-    cursor.executemany("INSERT INTO taxonomy_tmp(taxonomy_id, uniprot_id) values (?,?)", get_files())
-    cursor.execute('DROP INDEX IF EXISTS uni_index;')
-    cursor.execute('CREATE INDEX uni_index ON taxonomy_tmp(uniprot_id, taxonomy_id);')
-    cursor.execute('DROP TABLE IF EXISTS taxonomy;')
-    cursor.execute('ALTER TABLE taxonomy_tmp RENAME TO taxonomy;')
-    sqlite_conn.commit()
+        # Set up taxonomy<->uniprot DB
+        cursor.execute('DROP TABLE IF EXISTS taxonomy_tmp;')
+        cursor.execute('CREATE TABLE taxonomy_tmp (taxonomy_id text, uniprot_id text);')
+        cursor.executemany("INSERT INTO taxonomy_tmp(taxonomy_id, uniprot_id) values (?,?)",
+                           get_files(args.alphafold_path))
+        cursor.execute('DROP INDEX IF EXISTS uni_index;')
+        cursor.execute('CREATE INDEX uni_index ON taxonomy_tmp(uniprot_id, taxonomy_id);')
+        cursor.execute('DROP TABLE IF EXISTS taxonomy;')
+        cursor.execute('ALTER TABLE taxonomy_tmp RENAME TO taxonomy;')
+        sqlite_conn.commit()
 
-    # Set up the PDB<->uniprot DB
-    cursor.execute('DROP TABLE IF EXISTS pdb_tmp;')
-    cursor.execute('CREATE TABLE pdb_tmp (pdb_id text, uniprot_id text);')
-    cursor.executemany("INSERT INTO pdb_tmp(pdb_id, uniprot_id) values (?,?)", get_pdb_mappings())
-    cursor.execute('DROP INDEX IF EXISTS pdb_index;')
-    cursor.execute('CREATE INDEX pdb_index ON pdb_tmp (pdb_id, uniprot_id);')
-    cursor.execute('DROP TABLE IF EXISTS pdb;')
-    cursor.execute('ALTER TABLE pdb_tmp RENAME TO pdb;')
-    sqlite_conn.commit()
+        # Set up the PDB<->uniprot DB
+        cursor.execute('DROP TABLE IF EXISTS pdb_tmp;')
+        cursor.execute('CREATE TABLE pdb_tmp (pdb_id text, uniprot_id text);')
+        cursor.executemany("INSERT INTO pdb_tmp(pdb_id, uniprot_id) values (?,?)", get_pdb_mappings(args.download_pdb))
+        cursor.execute('DROP INDEX IF EXISTS pdb_index;')
+        cursor.execute('CREATE INDEX pdb_index ON pdb_tmp (pdb_id, uniprot_id);')
+        cursor.execute('DROP TABLE IF EXISTS pdb;')
+        cursor.execute('ALTER TABLE pdb_tmp RENAME TO pdb;')
+        sqlite_conn.commit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('alphafold_path',
+                        action='store',
+                        default='/extra/alphafoldorig/proteomes/',
+                        help='Where the source AlphaFold proteomes folder is.')
+    parser.add_argument('-d', '--download',
+                        action='store_true',
+                        dest='download_pdb',
+                        help='Force re-download the PDB index before processing')
+    args = parser.parse_args()
+
+    create_or_update_sqlite(args)
