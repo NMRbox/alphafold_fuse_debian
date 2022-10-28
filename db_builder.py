817bbf63ee8e5fb878e6783cd33450966f2f0bde
Jon Wedell <jon@jonwedell.com>
2022-10-28 11:46:59 -0500

    Implement code to index the exact position of files in the tar archives.

--- db_builder.py
+++ db_builder.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import gzip
+import multiprocessing
 import os
 import sqlite3
 import subprocess
@@ -8,23 +9,49 @@ import tarfile
 from typing import Union, Literal
 
 
-def get_files(root_dir: str):
+def round_to_512(number):
+    if number == 0:
+        return number
+
+    remainder = number % 512
+
+    if remainder == 0:
+        return number
+
+    return number + 512 - remainder
+
+
+def get_files_from_tar(entry: tarfile.TarInfo):
+    offset = 0
+    split = entry.name.split('-')
+    taxonomy_id = split[2]
+    chunk = split[3].split("_")[0]
+    print(f'Working on... {taxonomy_id}-{chunk}')
+
+    data = subprocess.check_output(['/usr/bin/tar', '--list', '--verbose', '-f', entry.path])
+    for line in data.decode().split('\n'):
+        parts = line.split()
+        if parts:
+            size = int(parts[2])
+            if parts[5].endswith('-F1-model_v3.cif.gz'):
+                yield taxonomy_id, chunk, parts[5].replace('-F1-model_v3.cif.gz', ''), offset, size
+            offset += size + 512
+            offset = round_to_512(offset)
+
+    # with tarfile.open(entry.path) as tf:
+    #     for file in tf.getmembers():
+    #         if file.name.endswith('-F1-model_v3.cif.gz'):
+    #             if file.name.replace('-F1-model_v3.cif.gz', '') == 'AF-A0A1Q1MKJ4':
+    #                 yield taxonomy_id, chunk, file.name.replace('-F1-model_v3.cif.gz', ''), file.offset, file.size
+
+
+def index_files(root_dir: str):
     # Populate DB
     with os.scandir(root_dir) as it:
         for entry in it:
             if entry.name.endswith('.tar'):
-                taxonomy_id = entry.name.split('-')[2]
-                print(f'Working on... {taxonomy_id}')
-
-                # data = subprocess.check_output(['/usr/bin/tar', '--list', '-f', entry.path])
-                # for file in data.decode().split('\n'):
-                #     if file.endswith('-F1-model_v3.cif.gz'):
-                #         yield taxonomy_id, file.replace('-F1-model_v3.cif.gz', '')
-
-                with tarfile.open(entry.path) as tf:
-                    for file in tf.getmembers():
-                        if file.name.endswith('-F1-model_v3.cif.gz'):
-                            yield taxonomy_id, file.name.replace('-F1-model_v3.cif.gz', '')
+                for record in get_files_from_tar(entry):
+                    yield record
 
 
 def get_id_mappings(download=False, action: Union[Literal['pdb'], Literal['uniprot']] = 'uniprot'):
@@ -53,31 +80,34 @@ def create_or_update_sqlite(args: argparse.Namespace):
         # Set up taxonomy<->uniprot DB
         print("Doing Uniprot<->Taxonomy ID")
         cursor.execute('DROP TABLE IF EXISTS taxonomy_tmp;')
-        cursor.execute('CREATE TABLE taxonomy_tmp (taxonomy_id text, uniprot_id text);')
-        cursor.executemany("INSERT INTO taxonomy_tmp(uniprot_id, taxonomy_id) values (?,?)",
-                           get_id_mappings(args.download_pdb, 'uniprot'))
+        cursor.execute('CREATE TABLE taxonomy_tmp (taxonomy_id text, chunk number, uniprot_id text, offset numeric, '
+                       'size numeric);')
+        cursor.executemany("INSERT INTO taxonomy_tmp(taxonomy_id, chunk, uniprot_id, offset, size) values (?,?,?,?,?)",
+                           index_files(args.alphafold_path))
         cursor.execute('DROP INDEX IF EXISTS uni_index;')
-        cursor.execute('CREATE UNIQUE INDEX uni_index ON taxonomy_tmp(uniprot_id, taxonomy_id);')
+        print('Building index...')
+        cursor.execute('CREATE UNIQUE INDEX uni_index ON taxonomy_tmp(uniprot_id, taxonomy_id, chunk);')
         cursor.execute('DROP TABLE IF EXISTS taxonomy;')
         cursor.execute('ALTER TABLE taxonomy_tmp RENAME TO taxonomy;')
         sqlite_conn.commit()
 
-        # Set up the PDB<->uniprot DB
-        print("Doing PDB<->UniProt")
-        cursor.execute('DROP TABLE IF EXISTS pdb_tmp;')
-        cursor.execute('CREATE TABLE pdb_tmp (pdb_id text, uniprot_id text);')
-        cursor.executemany("INSERT INTO pdb_tmp(pdb_id, uniprot_id) values (?,?)",
-                           get_id_mappings(args.download_pdb, 'pdb'))
-        cursor.execute('DROP INDEX IF EXISTS pdb_index;')
-        cursor.execute('CREATE UNIQUE INDEX pdb_index ON pdb_tmp (pdb_id, uniprot_id);')
-        cursor.execute('DROP TABLE IF EXISTS pdb;')
-        cursor.execute('ALTER TABLE pdb_tmp RENAME TO pdb;')
-        sqlite_conn.commit()
+        # # Set up the PDB<->uniprot DB
+        # print("Doing PDB<->UniProt")
+        # cursor.execute('DROP TABLE IF EXISTS pdb_tmp;')
+        # cursor.execute('CREATE TABLE pdb_tmp (pdb_id text, uniprot_id text);')
+        # cursor.executemany("INSERT INTO pdb_tmp(pdb_id, uniprot_id) values (?,?)",
+        #                    get_id_mappings(args.download_pdb, 'pdb'))
+        # cursor.execute('DROP INDEX IF EXISTS pdb_index;')
+        # cursor.execute('CREATE UNIQUE INDEX pdb_index ON pdb_tmp (pdb_id, uniprot_id);')
+        # cursor.execute('DROP TABLE IF EXISTS pdb;')
+        # cursor.execute('ALTER TABLE pdb_tmp RENAME TO pdb;')
+        # sqlite_conn.commit()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--alphafold-path',
+                        dest='alphafold_path',
                         action='store',
                         default='/extra/alphafoldorig/proteomes/',
                         help='Where the source AlphaFold proteomes folder is.')
